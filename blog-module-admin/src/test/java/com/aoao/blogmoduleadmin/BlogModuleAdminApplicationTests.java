a77c598dc3197e25ced27fa3dab40a54cabb1d28
Aoao <931203821@qq.com>
2025-07-06 19:52:33 +0800

    init: blog初始工程

---
+++ blog-module-admin/src/test/java/com/aoao/blogmoduleadmin/BlogModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoao.blogmoduleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogModuleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
